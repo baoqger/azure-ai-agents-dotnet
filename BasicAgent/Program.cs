3d1693b9fe68c36511cb44d4697aff4e08941d1b
Jingjing Bao <JBao6@slb.com>
2025-09-12 08:25:45 +0800

    add

---
+++ BasicAgent/Program.cs
@@ -0,0 +1,76 @@
+﻿using Azure;
+using Azure.AI.Agents.Persistent;
+using Azure.AI.Projects;
+using Azure.Identity;
+using DotNetEnv;
+
+Env.Load();
+
+var projectEndpoint = Environment.GetEnvironmentVariable("PROJECT_ENDPOINT");
+var modelDeploymentName = Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME");
+
+var agentClient = new PersistentAgentsClient(
+    projectEndpoint,
+    new DefaultAzureCredential());
+
+PersistentAgent agent = agentClient.Administration.CreateAgent(
+    model: modelDeploymentName,
+    name: "Math Tutor",
+    instructions: "You are a personal electronics tutor. Write and run code to answer questions.",
+    tools: [new CodeInterpreterToolDefinition()]);
+
+PersistentAgentThread thread = agentClient.Threads.CreateThread();
+PersistentThreadMessage message = agentClient.Messages.CreateMessage(
+    thread.Id,
+    MessageRole.User,
+    "What is the impedance formula?");
+
+ThreadRun agentRun = agentClient.Runs.CreateRun(
+    threadId: thread.Id,
+    agent.Id,
+    additionalMessages: [
+        new ThreadMessageOptions(
+            role: MessageRole.Agent,
+            content: "E=mc^2"
+        ),
+        new ThreadMessageOptions(
+            role: MessageRole.User,
+            content: "What is the impedance formula?"
+        ),
+    ]
+);
+
+do
+{
+    Thread.Sleep(TimeSpan.FromMilliseconds(500));
+    agentRun = agentClient.Runs.GetRun(thread.Id, agentRun.Id);
+}
+while (agentRun.Status == RunStatus.Queued
+    || agentRun.Status == RunStatus.InProgress);
+
+Pageable<PersistentThreadMessage> messages = agentClient.Messages.GetMessages(thread.Id, order: ListSortOrder.Ascending);
+
+foreach (PersistentThreadMessage threadMessage in messages)
+{
+    Console.Write($"{threadMessage.CreatedAt:yyyy-MM-dd HH:mm:ss} - {threadMessage.Role,10}: ");
+    foreach (MessageContent contentItem in threadMessage.ContentItems)
+    {
+        if (contentItem is MessageTextContent textItem)
+        {
+            Console.Write(textItem.Text);
+        }
+        else if (contentItem is MessageImageFileContent imageFileItem)
+        {
+            Console.Write($"<image from ID: {imageFileItem.FileId}");
+        }
+        Console.WriteLine();
+    }
+}
+
+// NOTE: Comment out these two lines if you plan to reuse the agent later.
+agentClient.Threads.DeleteThread(thread.Id);
+agentClient.Administration.DeleteAgent(agent.Id);
+
+
+
+
